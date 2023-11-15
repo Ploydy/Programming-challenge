dd8a398c2ceaab1342ebe39e3d0c9695a7ec54e9
Ploydy <Lfecarballo20@gmail.com>
2023-11-15 10:55:04 +0800

    +4

--- FINISH-PRODUCT/index.js
+++ FINISH-PRODUCT/index.js
@@ -10,7 +10,7 @@ console.log('1. Write a program that will display the even numbers from 1 to 100
 for (let i = 2; i <= 100; i += 2) {
   console.log(' ' + i)
 };
- 
+
 
 // 2 ----------------------------------------------------------------------------------------------------
 
@@ -19,4 +19,82 @@ console.log('2. Write a program that will compute for and display the sum of the
 for (let i = 3; i <= 100; i += 3) {
   console.log(' ' + i);
 };
- 
+
+// 3-----------------------------------------------------------------------------------------------------
+
+console.log('3. Write a program that will compute and display the sum of the powers of 2 from the first power to the nth power, where n is a nonnegative integer given by the user.');
+
+function sumOfPowersOf2(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += Math.pow(2, i);
+  }
+  return sum;
+}
+
+// Prompt the user for input
+const n = parseInt(prompt("Enter a nonnegative integer: "));
+
+if (!isNaN(n) && n >= 0) {
+  const result = sumOfPowersOf2(n);
+  console.log(result);
+}
+
+// 8-------------------------------------------------------------------------------------------------
+console.log('8. Write a program that reverses an input number.')
+
+
+let i = prompt('Input value: ')
+
+const reversedNum = i =>
+  parseFloat(i.toString().split('').reverse().join('')) * Math.sign(i)
+console.log(reversedNum(i))
+
+
+// 10--------------------------------------------------------------------------------------------------------
+console.log('10. Given the value of n from the user, write a program that will display the first n even numbers. ')
+
+function displayEvenNumbers(n) {
+  if (n <= 0) {
+    console.log("Enter a positive integer.");
+    return;
+  }
+
+  let count = 0;
+  let evenNumber = 2;
+
+  while (count < n) {
+    console.log(evenNumber);
+    evenNumber += 2;
+    count++;
+  }
+
+}
+const n = prompt("Enter a value: ");
+displayEvenNumbers(parseInt(n));
+
+
+// 11-------------------------------------------------------------------------------------------------
+console.log('11. Write a program that accepts a number n and displays the sum of even numbers and the sum of odd numbers from 1 to n.')
+
+function sumOfEvenAndOddNumbers(n) {
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      // Even number
+      sumEven += i;
+    } else {
+      // Odd number
+      sumOdd += i;
+    }
+  }
+
+  console.log(`Sum of even numbers from 1 to ${n}: ${sumEven}`);
+  console.log(`Sum of odd numbers from 1 to ${n}: ${sumOdd}`);
+}
+
+
+const n = prompt("Enter a number (n):");
+sumOfEvenAndOddNumbers(parseInt(n));
--- index.js
+++ index.js
@@ -3,44 +3,6 @@ import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
 
 
-// 1 
-
-console.log('1. Write a program that will display the even numbers from 1 to 100.')
-/* 
-for (let i = 2; i <= 100; i += 2) {
-  console.log(' ' + i)
-};
- */
-// 2
-
-console.log('2. Write a program that will compute for and display the sum of the numbers divisible by 3, ranging from 1 to 100.')
-/* 
-for (let i = 3; i <= 100; i += 3) {
-  console.log(' ' + i);
-};
- */
-// 3--------------------------------------------------------------------
-
-console.log('3. Write a program that will compute and display the sum of the powers of 2 from the first power to the nth power, where n is a nonnegative integer given by the user.');
-
-/* 
-function sumOfPowersOf2(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += Math.pow(2, i);
-  }
-  return sum;
-}
-
-// Prompt the user for input
-const n = parseInt(prompt("Enter a nonnegative integer: "));
-
-if (!isNaN(n) && n >= 0) {
-  const result = sumOfPowersOf2(n);
-  console.log(result);
-}
-*/
-
 // 4------------------------------------------------------------------------
 //console.log('4. Write a program that will compute for the following given n & x:');
 
@@ -105,16 +67,7 @@ for (let A = 1; A <= 100; A++) {
   }
 }
 */
-// 8-------------------------------------------------------------------------------------------------
-console.log('8. Write a program that reverses an input number.')
 
-/* 
-let i = prompt('Input value: ')
-
-const reversedNum = i => 
-parseFloat(i.toString().split('').reverse().join('')) * Math.sign(i)
-console.log(reversedNum(i))
-*/
 // 9-------------------------------------- --------------------------------------------------------------
 console.log('9. Write a program that gets as input a binary number and outputs its corresponding decimal equivalent. Declare your integer variables as long. A long declaration accommodates 8 digits for a number.')
 /* 
@@ -137,53 +90,10 @@ const binaryNumber = prompt('binary input: ')
 const decimalEquivalent = binaryToDecimal(binaryNumber);
 console.log(`Decimal equivalent is: ${decimalEquivalent}`);
 */
-// 10--------------------------------------------------------------------------------------------------------
-console.log('10. Given the value of n from the user, write a program that will display the first n even numbers. ')
-/* 
-function displayEvenNumbers(n) {
-  if (n <= 0) {
-    console.log("Please enter a positive integer.");
-    return;
-  }
 
-  let count = 0;
-  let evenNumber = 2;
 
-  while (count < n) {
-    console.log(evenNumber);
-    evenNumber += 2;
-    count++;
-  }
-}
 
-const n = prompt("Enter a value: ");
-displayEvenNumbers(parseInt(n)); 
-*/
-// 11-------------------------------------------------------------------------------------------------
-console.log('11. Write a program that accepts a number n and displays the sum of even numbers and the sum of odd numbers from 1 to n.')
-
-/* function sumOfEvenAndOddNumbers(n) {
-  let sumEven = 0;
-  let sumOdd = 0;
-
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 === 0) {
-      // Even number
-      sumEven += i;
-    } else {
-      // Odd number
-      sumOdd += i;
-    }
-  }
-
-  console.log(`Sum of even numbers from 1 to ${n}: ${sumEven}`);
-  console.log(`Sum of odd numbers from 1 to ${n}: ${sumOdd}`);
-}
-
-// Example usage:
-const n = prompt("Enter a number (n):"); // Prompt the user for the value of n
-sumOfEvenAndOddNumbers(parseInt(n));
- */
+ 
 // 12
 console.log('12. Write a program that will compute for axgiven real value a and positive integer x')
 
